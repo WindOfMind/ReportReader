60f3b9a9459ba207d884dfcbdac23d82be447da0
Dmitriy Aflitonov <aflitonovdv@gmail.com>
2020-12-12 14:34:14 +0200

    Add console solution
    Add VS solution with console project and support running the app with options

---
+++ ReportReaderApp/AppOptions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace ReportReaderApp
+{
+    public class AppOptions
+    {
+        public string Path { get; protected set; }
+
+        public bool SortByStartDate { get; protected set; }
+
+        public string ProjectId { get; protected set; }
+
+        private AppOptions()
+        {
+        }
+
+        public static AppOptions FromArguments(string[] args)
+        {
+            if (args == null)
+                throw new ArgumentNullException(nameof(args));
+
+            return new AppOptions
+            {
+                Path = ReadValue(args, "--file"),
+                SortByStartDate = args.Contains("--sortByStartDate"),
+                ProjectId = ReadValue(args, "--project")
+            };
+        }
+
+        private static string ReadValue(string[] args, string name)
+        {
+            int position = Array.IndexOf(args, name);
+            bool canReadValue = position + 1 > args.Length - 1;
+
+            string value = position >= 0 && canReadValue
+                ? args[position + 1]
+                : string.Empty;
+
+            if (value.StartsWith("--"))
+                return string.Empty;
+
+            return value;
+        }
+    }
+}
---
+++ ReportReaderApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReportReaderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = AppOptions.FromArguments(args);
+
+            if (string.IsNullOrEmpty(options.Path))
+            {
+                Console.WriteLine("Please specify a path for reading a report.");
+                Console.WriteLine("Usage: ReportReaderApp --file <path>");
+
+                return;
+            }
+        }
+    }
+}
